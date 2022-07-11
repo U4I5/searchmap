448fcfed86d9d8fbce37d24185d0ffb99ac3059a
U4I5 <u.soglondey@gmail.com>
2022-07-11 09:07:15 +0200

    nmap3

--- smap.py
+++ smap.py
@@ -1,19 +1,15 @@
 #!/bin/python3
 #coded by U4I5 For pentesting
-import os
-import subprocess
-
-kali = os.system
+import nmap3
+searchmap = nmap3.Nmap()
 #Target Ip address
-Iptarget = input('[+] Target: ')
+Ip = input('[+] Target: ')
+
 #Interface
-Iface = input("[+] Iface: ")
+#Iface = input("[+] Iface: ")
 
-if Iface == 0:
-    Iface = "eth0"
+# Scan Top 1000 ports with arguments : -Sv -sC 
+results = searchmap.scan_top_ports(target=Ip, args="-sV" "-sC")
 
-#1- fast Scan nmap
-kali("nmap" + " " "-sV" + " " "-sC"  + " " 'T4' + " " format(Iptarget) )
 
-#2- Full Scan nmap  
-# kali(f"nmap" + "-p-" "-sV" + "-sC" +  str('T4')+ {Iptarget} + "-oN" + {Iptarget}.txt)
+
